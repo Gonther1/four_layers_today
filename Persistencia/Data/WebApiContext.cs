6e75f86f576053536fe662eafcf74e5bd1a1fe00
Gonther1 <126286883+Gonther1@users.noreply.github.com>
2023-11-07 13:20:09 -0500

    feat: :sparkles: Create file context
    @Gonther1

---
+++ Persistencia/Data/WebApiContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Dominio.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dominio.Data;
+
+public partial class WebApiContext : DbContext
+{
+    public WebApiContext()
+    {
+    }
+
+    public WebApiContext(DbContextOptions<WebApiContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<City> Cities { get; set; }
+
+    public virtual DbSet<Country> Countries { get; set; }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    public virtual DbSet<PersonType> PersonTypes { get; set; }
+
+    public virtual DbSet<State> States { get; set; }
+
+    /* protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySql("server=localhost;user=root;password=123456;database=proyecto", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.34-mysql")); */
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+
+    /* partial void OnModelCreatingPartial(ModelBuilder modelBuilder); */
+}
