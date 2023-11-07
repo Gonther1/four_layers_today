6f6ff24ab08a387c5d9598dd5e91ecd5faa9aa7c
Gonther1 <126286883+Gonther1@users.noreply.github.com>
2023-11-07 13:19:43 -0500

    feat: :sparkles: Create all files configuration
    @Gonther1

---
+++ Persistencia/Data/Configuration/CountryConfiguration.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dominio.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistencia.Data.Configuration;
+
+public class CountryConfiguration : IEntityTypeConfiguration<Country>
+{
+    public void Configure(EntityTypeBuilder<Country> builder)
+    {
+        builder.HasKey(e => e.Id).HasName("PRIMARY");
+
+        builder.ToTable("country");
+
+        builder.Property(e => e.Name)
+            .HasMaxLength(50)
+            .HasColumnName("name");
+    }
+}
---
+++ Persistencia/Data/Configuration/PersonTypeConfiguration.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dominio.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistencia.Data.Configuration;
+
+public class PersonTypeConfiguration : IEntityTypeConfiguration<PersonType>
+{
+    public void Configure(EntityTypeBuilder<PersonType> builder)
+    {
+        builder.HasKey(e => e.Id).HasName("PRIMARY");
+
+        builder.ToTable("person_type");
+
+        builder.Property(e => e.Name)
+            .HasMaxLength(50)
+            .HasColumnName("name");
+    }
+}
