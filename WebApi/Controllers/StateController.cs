fb865e73341378f8acf1f662fd757c9f8f91cc44
Gonther1 <luisandresalvarezsilva@gmail.com>
2023-11-09 18:28:59 -0500

    feat: :sparkles: Configurate StateController
    @Gonther1

--- WebApi/Controllers/StateController.cs
+++ WebApi/Controllers/StateController.cs
@@ -2,14 +2,94 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using Dominio.Entities;
+using Dominio.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using WebApi.Dtos;
 
-namespace WebApi.Controllers
+namespace WebApi.Controllers;
+public class StateController : BaseController
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class StateController : ControllerBase
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+    public StateController(IUnitOfWork unitOfWork, IMapper mapper)
     {
-        
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<IEnumerable<StateDto>>> Get()
+    {
+        var entity = await _unitOfWork.States.GetAllAsync();
+        return _mapper.Map<List<StateDto>>(entity);
+    }
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<State>> Post(StateDto entityDto)
+    {
+        var entity = _mapper.Map<State>(entityDto);
+        _unitOfWork.States.Add(entity);
+        await _unitOfWork.SaveAsync();
+        if (entity == null)
+        {
+            return BadRequest();
+        }
+        entityDto.Id = entity.Id;
+        return CreatedAtAction(nameof(Post), new { id = entityDto.Id }, entityDto);
+    }
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<StateDto>> Get(int id)
+    {
+        var entity = await _unitOfWork.States.GetByIdAsync(id);
+        if (entity == null)
+        {
+            return NotFound();
+        }
+        return _mapper.Map<StateDto>(entity);
+    }
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<StateDto>> Put(int id, [FromBody] StateDto entityDto)
+    {
+        var entity = _mapper.Map<State>(entityDto);
+        if (entity.Id == 0)
+        {
+            entity.Id = id;
+        }
+        if (entity.Id != id)
+        {
+            return BadRequest();
+        }
+        if (entity == null)
+        {
+            return NotFound();
+        }
+        entityDto.Id = entity.Id;
+        _unitOfWork.States.Update(entity);
+        await _unitOfWork.SaveAsync();
+        return entityDto;
+    }
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult> Delete(int id)
+    {
+        var entity = await _unitOfWork.States.GetByIdAsync(id);
+        if (entity == null)
+        {
+            return NotFound();
+        }
+        _unitOfWork.States.Remove(entity);
+        await _unitOfWork.SaveAsync();
+        return NoContent();
     }
 }
