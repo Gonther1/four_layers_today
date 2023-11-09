eed95669b404b7f3d731867c7ace292c81934282
Gonther1 <luisandresalvarezsilva@gmail.com>
2023-11-09 18:27:48 -0500

    feat: :sparkles: Create mapping profiles file
    @Gonther1

---
+++ WebApi/Profiles/MappingProfiles.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Dominio.Entities;
+using WebApi.Dtos;
+
+namespace WebApi.Profiles;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<City, CityDto>().ReverseMap();  
+        CreateMap<Country, CountryDto>().ReverseMap();  
+        CreateMap<Customer, CustomerDto>().ReverseMap();  
+        CreateMap<PersonType, PersonTypeDto>().ReverseMap();  
+        CreateMap<State, StateDto>().ReverseMap();  
+    }
+}
