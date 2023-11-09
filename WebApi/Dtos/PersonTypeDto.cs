f9cba617d55d0f8c1854057dc2070db0d36a12f0
Gonther1 <126286883+Gonther1@users.noreply.github.com>
2023-11-09 13:13:36 -0500

    feat: :sparkles: Create all Dtos
    @Gonther1

---
+++ WebApi/Dtos/PersonTypeDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Dtos;
+
+public class PersonTypeDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+}
